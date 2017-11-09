209eade0940723b1bad168032c14e94e7da01164
jamie <jrkeye2@gmail.com>
2017-11-08 19:23:12 -0500

    -a

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+   
+    // An alert letting visitors that the email list isn't implement yet
+    
+    $( "#ebtn" ).click(function(){
+        alert( "Coming Soon!" );
+    });
+    
+});
